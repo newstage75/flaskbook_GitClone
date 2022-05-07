6e1260ef61160e91b1c74a44f870d414e9259e7a
github newstage75 <rightkingdxanntume@gmail.com>
2022-05-07 10:07:58 +0900

    UserImageTagsモデルの作成

--- apps/detector/models.py
+++ apps/detector/models.py
@@ -12,3 +12,15 @@ class UserImage(db.Model):
     is_detected = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+
+# db.Modelを継承したUserImageクラスを作成する
+class UserImageTag(db.Model):
+    # テーブルを指名する
+    __tablename__ = "user_image_tags"
+    id = db.Column(db.Integer, primary_key=True)
+    # user_imgae_idはuser_imagesテーブルのidカラムの外部キーとして設定する。
+    user_image_id = db.Column(db.String, db.ForeignKey("user_images.id"))
+    tag_name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
